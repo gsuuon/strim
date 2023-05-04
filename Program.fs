4e36595ad0c339cadb90e83d807167cb42099875
Steven Sun <gsuuon@gmail.com>
2023-05-04 01:56:33 -0500

    chore: fantomas

--- Program.fs
+++ Program.fs
@@ -4,71 +4,66 @@ open System.Text.RegularExpressions
 
 Console.OutputEncoding <- Text.Encoding.UTF8
 
-type ProcResult = {
-  out : string
-  err : string
-  code : int
-}
+type ProcResult = { out: string; err: string; code: int }
 
-/// NOTE this will stall forever if proc waits for input with a message / and may not show message (if it isn't newlined)
 let exec handleOut handleErr (cmd: string) =
-  let processStartInfo = new ProcessStartInfo()
+    let processStartInfo = new ProcessStartInfo()
 
-  processStartInfo.FileName <- "cmd.exe"
-  processStartInfo.Arguments <- sprintf "/C %s" cmd
-  processStartInfo.UseShellExecute <- false
-  processStartInfo.RedirectStandardOutput <- true
-  processStartInfo.RedirectStandardError <- true
-  processStartInfo.RedirectStandardInput <- true
+    processStartInfo.FileName <- "cmd.exe"
+    processStartInfo.Arguments <- sprintf "/C %s" cmd
+    processStartInfo.UseShellExecute <- false
+    processStartInfo.RedirectStandardOutput <- true
+    processStartInfo.RedirectStandardError <- true
+    processStartInfo.RedirectStandardInput <- true
 
-  let proc = new Process()
-  proc.StartInfo <- processStartInfo
-  proc.Start() |> ignore
+    let proc = new Process()
+    proc.StartInfo <- processStartInfo
+    proc.Start() |> ignore
 
-  let mutable errLines = []
-  let mutable outLines = []
-
-  async {
-    let stdin = proc.StandardInput
+    let mutable errLines = []
+    let mutable outLines = []
 
     async {
-      use errReader = proc.StandardError
+        let stdin = proc.StandardInput
 
-      while not proc.HasExited do
-        if not errReader.EndOfStream then
-          let err = errReader.ReadLine()
-          errLines <- errLines @ [err]
-          handleErr err
-    } |> Async.Start
+        async {
+            use errReader = proc.StandardError
 
-    async {
-      while not proc.HasExited do
-        let line = Console.ReadLine()
-        stdin.WriteLine(line)
-    } |> Async.Start
+            while not proc.HasExited do
+                if not errReader.EndOfStream then
+                    let err = errReader.ReadLine()
+                    errLines <- errLines @ [ err ]
+                    handleErr err
+        }
+        |> Async.Start
 
-    async {
-      use reader = proc.StandardOutput
+        async {
+            while not proc.HasExited do
+                let line = Console.ReadLine()
+                stdin.WriteLine(line)
+        }
+        |> Async.Start
 
-      while not proc.HasExited do
-        if not reader.EndOfStream then
-          let output = reader.ReadLine()
-          outLines <- outLines @ [output]
-          handleOut output
-    } |> Async.Start
-  } |> Async.Start
+        async {
+            use reader = proc.StandardOutput
 
-  Console.CancelKeyPress.Add(fun e ->
-    e.Cancel <- true
-  )
+            while not proc.HasExited do
+                if not reader.EndOfStream then
+                    let output = reader.ReadLine()
+                    outLines <- outLines @ [ output ]
+                    handleOut output
+        }
+        |> Async.Start
+    }
+    |> Async.Start
 
-  proc.WaitForExit() |> ignore
+    Console.CancelKeyPress.Add(fun e -> e.Cancel <- true)
 
-  {
-    out = String.concat "\n" outLines
-    err = String.concat "\n" errLines
-    code = proc.ExitCode
-  }
+    proc.WaitForExit() |> ignore
+
+    { out = String.concat "\n" outLines
+      err = String.concat "\n" errLines
+      code = proc.ExitCode }
 
 // We're sending everything except output file to stderr so stdout is just the output file name
 let show = exec (eprintfn "%s") (eprintfn "%s")
@@ -77,23 +72,18 @@ let hide = exec ignore ignore
 let sleep ms = Threading.Thread.Sleep 1000
 
 let twitchKey =
-  match Environment.GetEnvironmentVariable "TWITCH_STREAM_KEY" with
-  | null ->
-    failwithf "Missing TWITCH_STREAM_KEY"
-  | x -> x
+    match Environment.GetEnvironmentVariable "TWITCH_STREAM_KEY" with
+    | null -> failwithf "Missing TWITCH_STREAM_KEY"
+    | x -> x
 
 let ingestUrl =
-  let defaultUrl = "rtmp://live.twitch.tv/app"
+    let defaultUrl = "rtmp://live.twitch.tv/app"
 
-  match
-    Environment.GetCommandLineArgs()
-    |> Array.tryItem 1
-  with
-  | Some ingestUrl ->
-    ingestUrl
-  | None ->
-    printfn "Defaulting to %s" defaultUrl
-    defaultUrl
+    match Environment.GetCommandLineArgs() |> Array.tryItem 1 with
+    | Some ingestUrl -> ingestUrl
+    | None ->
+        printfn "Defaulting to %s" defaultUrl
+        defaultUrl
 
 printfn "Going live in 3.."
 sleep 1000
@@ -103,53 +93,41 @@ printfn "1.."
 sleep 1000
 printfn "🔦"
 
-type StreamProgress = {
-  bitrate : string
-  fps : string
-  uptime : string
-}
+type StreamProgress =
+    { bitrate: string
+      fps: string
+      uptime: string }
 
 let twitch cmd =
-  let trim (x: string) = x.Trim()
+    let trim (x: string) = x.Trim()
 
-  let parseStatus out =
-    Regex.Match
-      ( out
-      , ".+fps=(?<fps>[0-9 .]+).+size=(?<size>.+)time=(?<time>[0-9:.]+).*bitrate=(?<bitrate>.+/s)."
-      )
+    let parseStatus out =
+        Regex.Match(out, ".+fps=(?<fps>[0-9 .]+).+size=(?<size>.+)time=(?<time>[0-9:.]+).*bitrate=(?<bitrate>.+/s).")
 
-  let handleProgress =
-    let mutable started = false
+    let handleProgress =
+        let mutable started = false
 
-    fun out ->
-      match parseStatus out with
-      | capture when capture.Success -> 
-        if started = false
-          && trim capture.Groups.["size"].Value <> "0kb"
-        then
-          started <- true
-          printfn "🎥"
+        fun out ->
+            match parseStatus out with
+            | capture when capture.Success ->
+                if started = false && trim capture.Groups.["size"].Value <> "0kb" then
+                    started <- true
+                    printfn "🎥"
 
-        let v (name: string) = capture.Groups.[name].Value
+                let v (name: string) = capture.Groups.[name].Value
 
-        let status =
-          sprintf "[uptime %s] [fps %s] [bitrate %s]"
-            (v "time")
-            (v "fps")
-            (v "bitrate")
-            
-        Console.Write status
-        Console.Write "\r"
-      | _ -> ()
+                let status =
+                    sprintf "[uptime %s] [fps %s] [bitrate %s]" (v "time") (v "fps") (v "bitrate")
 
-  exec
-  <| eprintfn "%s"
-  <| handleProgress
-  <| cmd
-  |> ignore
+                Console.Write status
+                Console.Write "\r"
+            | _ -> ()
 
-  printfn ""
+    exec <| eprintfn "%s" <| handleProgress <| cmd |> ignore
 
-twitch $"""ffmpeg -f gdigrab -framerate 30 -i desktop -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 60 -c:a aac -b:a 128k -f flv {ingestUrl}/{twitchKey}"""
+    printfn ""
+
+twitch
+    $"""ffmpeg -f gdigrab -framerate 30 -i desktop -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 60 -c:a aac -b:a 128k -f flv {ingestUrl}/{twitchKey}"""
 
 printfn "🎬"
